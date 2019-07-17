b514fd7e98a70d924363136dd5a2e762877b77c4
quan005 <ralphealfreeman@gmail.com>
2019-07-16 17:57:27 -0700

    MVP Complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,32 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const GET_DATA_START = "GET_DATA_START"
+export const GET_DATA_SUCCESS = "GET_DATA_SUCCESS"
+export const GET_DATA_FAILED = "GET_DATA_FAILED"
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+export function getData() {
+    return(dispatch) => {
+        dispatch({type: GET_DATA_START})
+
+        axios
+            .get("https://swapi.co/api/people/")
+            .then(res => {
+                dispatch({
+                    type: GET_DATA_SUCCESS,
+                    payload: res.data
+                })
+            })
+            .catch(err => {
+                dispatch({
+                    type: GET_DATA_FAILED,
+                    payload: err
+                })
+            })
+    }
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,40 @@
-import /* we need our action types here*/ "../actions";
+/* we need our action types here*/
+import {GET_DATA_START, GET_DATA_SUCCESS, GET_DATA_FAILED} from "../actions";
+
 const initialState = {
-  characters: []
   // Array characters, Boolean fetching, null error.
+  characters: [],
+  fetchingData: false,
+  errorMessage: null,
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case GET_DATA_START: {
+      return {
+        ...state,
+        fetchingData: true
+      }
+    }
+
+    case GET_DATA_SUCCESS: {
+      return {
+        ...state,
+        fetchingData: false,
+        characters: action.payload.results,
+        errorMessage: null,
+      }
+    }
+
+    case GET_DATA_FAILED: {
+      return {
+        ...state
+      }
+    }
+
     default:
       return state;
   }
